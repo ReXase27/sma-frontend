d4eda1f47ddf78f439b0dc78ab96eb07c63726ef
ReXase27 <claukonstantin@hotmail.com>
2023-06-01 00:36:50 +0200

    a

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,24 +6,44 @@
     const { posts } = data;
 </script>
 
-<button
-    type="button"
-    class="bg-orange-400 text-white rounded-md p-4 text-center text-lg"
-    on:click={() => {
-        goto("/new");
-    }}>What's in your mind?</button
->
+<div class="grid grid-cols-3 gap-4 h-screen bg-[#333] text-white">
+    <div class="flex col-auto align-middle pl-8">
+        <ul>
+            <li>Profile</li>
+            <li>Settings</li>
+            <li>Logout</li>
+        </ul>
+    </div>
+    <div>
+        <button
+            type="button"
+            class="bg-orange-400 text-white rounded-md p-4 text-center text-lg"
+            on:click={() => {
+                goto("/new");
+            }}>What's in your mind?</button
+        >
+        <div>
+            {#if data}
+                {#if data.posts.length === 0}
+                    <p>No posts yet!</p>
+                {/if}
 
-{#if data}
-    {#if data.posts.length === 0}
-        <p>No posts yet!</p>
-    {/if}
-
-    {#each posts as post}
-        <h1>{post.authorId}</h1>
-        <h1>{post.title}</h1>
-        <p>{post.body}</p>
-    {/each}
-{:else}
-    <p>loading...</p>
-{/if}
+                {#each posts as post}
+                    <h1>{post.authorId}</h1>
+                    <h1>{post.title}</h1>
+                    <p>{post.body}</p>
+                {/each}
+            {:else}
+                <p>loading...</p>
+            {/if}
+        </div>
+    </div>
+    <div class="flex flex-col">
+        <h3>Trending</h3>
+        <ul>
+            <li>Post 1</li>
+            <li>Post 2</li>
+            <li>Post 3</li>
+        </ul>
+    </div>
+</div>
